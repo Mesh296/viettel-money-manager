89439b94487396092116b2ffdf4e889f86088af3
Mesh296 <minhhan181@gmail.com>
2025-05-14 01:14:29 +0300

    feat: init db

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv')
+const db = require('./src/providers/db.js');
+const sequelize = require('./src/providers/db.js');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.json('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`)
+});
+
+(async () => {
+    try {
+        await sequelize.sync({ force: false });
+        console.log('All models were synchronized successfully');
+    } catch (error) {
+        console.error("Error synchronizing models:", error);
+    }
+})();
