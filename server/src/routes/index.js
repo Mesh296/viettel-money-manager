68f07dd70bfa530a2633faf1474cfa10c88c9965
Mesh296 <minhhan181@gmail.com>
2025-05-17 11:59:32 +0300

    feat: create UsersCategories service, controller,  routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const authentication = require('../middlewares/authentication.js');
 const userRoutes = require('./users/index.js');
 const categoryRoutes = require('./categories');
+const userCategoryRoutes = require('./usersCategories');
 const router = express.Router();
 
 router.use('/users', userRoutes);
 router.use('/categories', categoryRoutes);
+router.use('/users-categories', userCategoryRoutes);
 
 module.exports = router;
